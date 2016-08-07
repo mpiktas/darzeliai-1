397ca6ef141979d39ea6c2371f78e5c4118cf34d
Lukasyo <lukas.kondrusevicius@gmail.com>
2016-08-07 12:03:54 +0300

    Vaikai laukiantys pagal amžių ir seniūnijas grafikai

--- main.R
+++ main.R
@@ -1,11 +1,10 @@
 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(zoo)
 library(ggplot2)
 
 #~~~Data input
-
+print("Data input")
 laukiantys <- read.csv(file="data/laukianciuju_eileje_ataskaita.csv",sep = ";",encoding="UTF-8")
 darzeliai <- read.csv(file="data/darzeliai.csv",encoding="UTF-8")
 grupes <- read.csv(file="data/grupes.csv",encoding="UTF-8")
@@ -13,9 +12,9 @@ istaigos <- read.csv(file="data/istaigos.csv",sep = ";", encoding="UTF-8")
 vaikai_grup <- read.csv(file="data/lankanciu_vaiku_ataskaita_pagal_grupes.csv",sep = ";", encoding="UTF-8")
 lankomumas <- read.csv(file="data/lankomumo_ziniarasciai_2016-07-01-2016-07-31.csv",sep = ";", encoding="UTF-8")
 prasymai <- read.csv(file="data/visi_prasymai.csv", encoding="UTF-8")
-
+print("Data input done")
 #~Data checks & corrections
-
+print("Data checks and corrections")
 #~~If there are duplicates between lankomumas and laukiantys, the corresponding row is removed from laukiantys
 
 seni.vaikai <- lankomumas[,c("Vaiko.Identifikacinis.Nr.","Darželio.pavadinimas")]
@@ -26,24 +25,50 @@ jau.gave <- visi.vaikai[which(duplicated(visi.vaikai$Vaiko.Identifikacinis.Nr.))
 jau.gave.id <- as.vector(jau.gave[,1])
 laukiantys <- laukiantys[-which(laukiantys$Vaiko.Identifikacinis.Nr. %in% jau.gave.id),]
 
+laukiantys$Vaiko.seniunija <- as.character(laukiantys$Vaiko.seniunija)
+laukiantys$Vaiko.seniunija[laukiantys$Vaiko.seniunija == "visos"] <- "Visos seniūnijos"
+laukiantys$Vaiko.seniunija[laukiantys$Vaiko.seniunija == ""] <- "Seniūnija nenurodyta"
+
+print("Data checks and corrections done")
+
 #~Laukiantys analysis
+
+print("Laukiantys analysis")
+
+#~~Kiek metų laukia savo eilės
 
 laukiantys$date <- substr(laukiantys$Prašymo.pateikimo.data,1,10)
 
 laukiantys$kiek.laukia <- (as.yearmon(Sys.Date()) - as.yearmon(laukiantys[,"date"]))
 
 ggplot(laukiantys, aes(x = kiek.laukia, fill = Prioritetas..deklaruotas.mieste.))  +
-  geom_histogram(binwidth = 0.25) +
-  xlab("Kiek metų jau laukia savo eilės šiai dienai") +
-  ylab("Vaikų skaičius")
+  geom_histogram(binwidth = 0.5) +
+  labs(x="Kiek metų laukia savo eilės",y="Vaikų skaičius",
+       fill="Gyvenamoji vieta deklaruota mieste")
+
+#~~Kiek metų laukia savo eilės pagal seniūnijas
 
 ggplot(laukiantys, aes(x = kiek.laukia, fill = Prioritetas..deklaruotas.mieste.)) +
-  geom_histogram(binwidth = 0.25) +
+  geom_histogram(binwidth = 0.5) +
   facet_wrap(~Vaiko.seniunija) + 
   ggtitle("Vaiko seniūnija") +
-  xlab("Kiek metų jau laukia savo eilės šiai dienai") +
-  ylab("Vaikų skaičius")
+  labs(x="Kiek metų laukia savo eilės",y="Vaikų skaičius",
+       fill="Gyvenamoji vieta deklaruota mieste")
 
+#~~Kokio amžiaus vaikai yra laukiančiųjų sąraše
+
+laukiantys$amzius <- (as.yearmon(Sys.Date()) - as.yearmon(laukiantys[,"Vaiko.gimimo.data"]))
+
+ggplot(laukiantys, aes(x = kiek.laukia, fill = Prioritetas..deklaruotas.mieste.))  +
+  geom_histogram(binwidth = 1) +
+  labs(x="Vaiko amžius",y="Vaikų skaičius",
+       fill="Gyvenamoji vieta deklaruota mieste")
+
+#~~Kokio amžiaus vaikai yra laukiančiųjų sąraše pagal seniūnijas
+
+
+
+print("Laukiantys analysis done")
 
 
 
